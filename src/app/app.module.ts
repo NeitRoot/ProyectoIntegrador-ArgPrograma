871a3bf2977fdf0fb41de7a2594a0dc41a98d3d8
NeitRoot <natya.vizcarra@gmail.com>
2022-07-12 22:59:21 -0300

    Primer header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgprogComponent } from './components/logo-argprog/logo-argprog.component';
+import { SocialmediaComponent } from './components/socialmedia/socialmedia.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoArgprogComponent
+    LogoArgprogComponent,
+    SocialmediaComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
