8095fb8c706c80952665dcfff65283f86d137662
NeitRoot <natya.vizcarra@gmail.com>
2022-07-31 01:09:26 -0300

    Cambios

--- src/app/model/persona.model.ts
+++ src/app/model/persona.model.ts
@@ -3,15 +3,15 @@ export class persona{
     nombre: string;
     apellido: string;
     img: string;
-    titulodepersona: string;
+    titutopersona: string;
     sobrepersona: string;
 
 
-    constructor(nombre: string, apellido: string, img: string, titulodepersona: string, sobrepersona: string){
+    constructor(nombre: string, apellido: string, img: string, titutopersona: string, sobrepersona: string){
         this.nombre = nombre;
         this.apellido = apellido;
         this.img = img;
-        this.titulodepersona = titulodepersona;
+        this.titutopersona = titutopersona;
         this.sobrepersona = sobrepersona;
 
 
